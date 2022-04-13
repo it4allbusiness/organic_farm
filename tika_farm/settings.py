1f497455bde6d218d334df9079cb33f71bfba518
it4allbusiness <102702830+it4allbusiness@users.noreply.github.com>
2022-04-12 20:48:22 -0400

    Update settings.py

--- tika_farm/settings.py
+++ tika_farm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1=baxet#yyno++@junhl7evy(&%u2kerp20^4$1pl)4fyn@a07'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
